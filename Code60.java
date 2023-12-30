011a4e9fa4fc28552cdb797dac84376a9325cbdb
unknown <modalaashwin>
2023-12-30 18:30:15 +0530

    Commit

---
+++ Code60.java
@@ -0,0 +1,17 @@
+public class Code60 {
+    int add(n){
+        System.out.println(n*n);
+    }
+    int add(n,n){
+                System.out.println(n*n);
+
+
+    }
+    public static void main(String args[]){
+
+        Code60 sc=new Code60();
+        sc.add(5);
+
+    }
+    
+}
